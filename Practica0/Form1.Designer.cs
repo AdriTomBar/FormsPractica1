141ae8a4aae5bab672365da98ec4ba5cd2220a93
AdriTomBar <145601607+esopom@users.noreply.github.com>
2024-10-23 09:44:25 +0200

    Agregar archivos de proyecto.

---
+++ Practica0/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Practica0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelDireccion = new System.Windows.Forms.Label();
+            this.labelCiudad = new System.Windows.Forms.Label();
+            this.labeFoto = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxDireccion = new System.Windows.Forms.TextBox();
+            this.buttonInsertarFoto = new System.Windows.Forms.Button();
+            this.BotoonValidar = new System.Windows.Forms.Button();
+            this.buttonLimpiar = new System.Windows.Forms.Button();
+            this.buttonSalir = new System.Windows.Forms.Button();
+            this.pictureBoxFoto = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formulario";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(64, 118);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(44, 13);
+            this.labelNombre.TabIndex = 1;
+            this.labelNombre.Text = "Nombre";
+            this.labelNombre.Click += new System.EventHandler(this.labelNombre_Click);
+            // 
+            // labelDireccion
+            // 
+            this.labelDireccion.AutoSize = true;
+            this.labelDireccion.Location = new System.Drawing.Point(64, 208);
+            this.labelDireccion.Name = "labelDireccion";
+            this.labelDireccion.Size = new System.Drawing.Size(52, 13);
+            this.labelDireccion.TabIndex = 2;
+            this.labelDireccion.Text = "Dirección";
+            this.labelDireccion.Click += new System.EventHandler(this.labelDireccion_Click);
+            // 
+            // labelCiudad
+            // 
+            this.labelCiudad.AutoSize = true;
+            this.labelCiudad.Location = new System.Drawing.Point(64, 298);
+            this.labelCiudad.Name = "labelCiudad";
+            this.labelCiudad.Size = new System.Drawing.Size(40, 13);
+            this.labelCiudad.TabIndex = 3;
+            this.labelCiudad.Text = "Ciudad";
+            this.labelCiudad.Click += new System.EventHandler(this.labelCiudad_Click);
+            // 
+            // labeFoto
+            // 
+            this.labeFoto.AutoSize = true;
+            this.labeFoto.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeFoto.Location = new System.Drawing.Point(410, 108);
+            this.labeFoto.Name = "labeFoto";
+            this.labeFoto.Size = new System.Drawing.Size(51, 23);
+            this.labeFoto.TabIndex = 4;
+            this.labeFoto.Text = "Foto";
+            this.labeFoto.Click += new System.EventHandler(this.labeFoto_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Madrid",
+            "Murcia",
+            "Valencia",
+            "Barcelona"});
+            this.comboBox1.Location = new System.Drawing.Point(152, 290);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(152, 115);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(121, 20);
+            this.textBoxNombre.TabIndex = 6;
+            this.textBoxNombre.TextChanged += new System.EventHandler(this.textBoxNombre_TextChanged);
+            // 
+            // textBoxDireccion
+            // 
+            this.textBoxDireccion.Location = new System.Drawing.Point(152, 201);
+            this.textBoxDireccion.Name = "textBoxDireccion";
+            this.textBoxDireccion.Size = new System.Drawing.Size(121, 20);
+            this.textBoxDireccion.TabIndex = 7;
+            this.textBoxDireccion.TextChanged += new System.EventHandler(this.textBoxDireccion_TextChanged);
+            // 
+            // buttonInsertarFoto
+            // 
+            this.buttonInsertarFoto.Location = new System.Drawing.Point(414, 168);
+            this.buttonInsertarFoto.Name = "buttonInsertarFoto";
+            this.buttonInsertarFoto.Size = new System.Drawing.Size(103, 53);
+            this.buttonInsertarFoto.TabIndex = 8;
+            this.buttonInsertarFoto.Text = "Insertar Foto";
+            this.buttonInsertarFoto.UseVisualStyleBackColor = true;
+            this.buttonInsertarFoto.Click += new System.EventHandler(this.buttonInsertarFoto_Click);
+            // 
+            // BotoonValidar
+            // 
+            this.BotoonValidar.Location = new System.Drawing.Point(425, 374);
+            this.BotoonValidar.Name = "BotoonValidar";
+            this.BotoonValidar.Size = new System.Drawing.Size(75, 23);
+            this.BotoonValidar.TabIndex = 9;
+            this.BotoonValidar.Text = "Validar";
+            this.BotoonValidar.UseVisualStyleBackColor = true;
+            this.BotoonValidar.Click += new System.EventHandler(this.BotoonValidar_Click);
+            // 
+            // buttonLimpiar
+            // 
+            this.buttonLimpiar.Location = new System.Drawing.Point(540, 374);
+            this.buttonLimpiar.Name = "buttonLimpiar";
+            this.buttonLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.buttonLimpiar.TabIndex = 10;
+            this.buttonLimpiar.Text = "Limpiar";
+            this.buttonLimpiar.UseVisualStyleBackColor = true;
+            this.buttonLimpiar.Click += new System.EventHandler(this.buttonLimpiar_Click);
+            // 
+            // buttonSalir
+            // 
+            this.buttonSalir.Location = new System.Drawing.Point(655, 374);
+            this.buttonSalir.Name = "buttonSalir";
+            this.buttonSalir.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalir.TabIndex = 11;
+            this.buttonSalir.Text = "Salir";
+            this.buttonSalir.UseVisualStyleBackColor = true;
+            this.buttonSalir.Click += new System.EventHandler(this.buttonSalir_Click);
+            // 
+            // pictureBoxFoto
+            // 
+            this.pictureBoxFoto.Location = new System.Drawing.Point(574, 108);
+            this.pictureBoxFoto.Name = "pictureBoxFoto";
+            this.pictureBoxFoto.Size = new System.Drawing.Size(133, 152);
+            this.pictureBoxFoto.TabIndex = 12;
+            this.pictureBoxFoto.TabStop = false;
+            this.pictureBoxFoto.Click += new System.EventHandler(this.pictureBoxFoto_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBoxFoto);
+            this.Controls.Add(this.buttonSalir);
+            this.Controls.Add(this.buttonLimpiar);
+            this.Controls.Add(this.BotoonValidar);
+            this.Controls.Add(this.buttonInsertarFoto);
+            this.Controls.Add(this.textBoxDireccion);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.labeFoto);
+            this.Controls.Add(this.labelCiudad);
+            this.Controls.Add(this.labelDireccion);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelDireccion;
+        private System.Windows.Forms.Label labelCiudad;
+        private System.Windows.Forms.Label labeFoto;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxDireccion;
+        private System.Windows.Forms.Button buttonInsertarFoto;
+        private System.Windows.Forms.Button BotoonValidar;
+        private System.Windows.Forms.Button buttonLimpiar;
+        private System.Windows.Forms.Button buttonSalir;
+        private System.Windows.Forms.PictureBox pictureBoxFoto;
+    }
+}
+
---
+++ Practica0/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonInsertarFoto_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+                pictureBoxFoto.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
+
+        private void buttonLimpiar_Click(object sender, EventArgs e)
+        {
+            textBoxDireccion.Text = string.Empty;
+            textBoxNombre.Text = string.Empty;
+            comboBox1.Text = string.Empty;
+            pictureBoxFoto.Image = null;
+
+        }
+
+        private void BotoonValidar_Click(object sender, EventArgs e)
+        {
+            if (textBoxNombre.Text == "" || textBoxDireccion.Text == "" || comboBox1.Text == "" || pictureBoxFoto == null)
+            {
+                MessageBox.Show("No has introducido todos los datos");
+            }
+            else MessageBox.Show("Validacion completada correctamente");
+        }
+
+        private void buttonSalir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxDireccion_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labeFoto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelCiudad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelDireccion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelNombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBoxFoto_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
